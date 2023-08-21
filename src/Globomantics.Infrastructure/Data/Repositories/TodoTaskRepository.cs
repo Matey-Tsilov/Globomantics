0cf244af4fe314f91854226abc2f51280889f186
Matey Tsilov <matey.tsilov@gateway.one>
2023-08-21 12:45:34 +0300

    implement the TodoTask repository

---
+++ src/Globomantics.Infrastructure/Data/Repositories/TodoTaskRepository.cs
@@ -0,0 +1,26 @@
+﻿using Globomantics.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Globomantics.Infrastructure.Data.Repositories;
+
+public class TodoTaskRepository : ToDoRepository<TodoTask>
+{
+
+    public TodoTaskRepository(GlobomanticsDbContext context) : base(context)
+    {
+    }
+
+    public override async Task AddAsync(TodoTask task)
+    {
+        var todoTask = DomainToDataMapping.MapToDoFromDomain<Domain.TodoTask, Data.Models.TodoTask>(task);
+
+        await Context.AddAsync(todoTask);
+    }
+
+    public override async Task<TodoTask> GetAsync(Guid id)
+    {
+        var data = await Context.TodoTasks.SingleAsync(t => t.Id == id);
+
+        return DataToDomainMapping.MapToDoFromData<Data.Models.TodoTask, Domain.TodoTask>(data);
+    }
+}
